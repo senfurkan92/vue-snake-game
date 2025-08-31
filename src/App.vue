b3e0d1008039b73b957f72aea0a6563f647a07c4
Furkan Sen <senfurkan92@gmail.com>
2025-08-31 22:14:11 +0300

    fc

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import tileManager from './components/TileManager.vue';
+
+</script>
+
+<template>
+  <tile-manager></tile-manager>
+</template>
---
+++ src/components/TileManager.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue'
+import { getTileDetail } from '../assets/tile-calc';
+
+const [tileSide, points] = getTileDetail()
+</script>
+
+<template>
+    <div style="position: relative;">
+        <template v-for="p, i in points" :key="i">
+            <div class="tile" :style="{
+                height: `${tileSide}px`,
+                width: `${tileSide}px`,
+                left: `${p.corner.w}px`,
+                top: `${p.corner.h}px`
+            }">
+            </div>
+            <div class="center" :style="{
+                height: `${tileSide}px`,
+                width: `${tileSide}px`,
+                left: `${p.center.w - tileSide/2}px`,
+                top: `${p.center.h - tileSide/2}px`
+            }">
+            </div>
+        </template>
+    </div>
+</template>
+
+<style scoped>
+.tile {
+    position: absolute;
+    background-color: beige;
+    border: 0.5px solid black;
+}
+
+.center {
+    position: absolute;
+    background-color: chocolate;
+    border-radius: 100%;
+}
+</style>
